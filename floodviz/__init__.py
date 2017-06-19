b8940e9e6651f5c74cf48cb8f2495831aaf2dcd9
Mary Bucknell <mbucknell@usgs.gov>
2017-06-19 16:00:58 -0500

    Initial project scaffolding

---
+++ floodviz/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+app = Flask(__name__.split()[0], instance_relative_config=True)
+
+# Loads configuration information from config.py and instance/config.py
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
+
+import floodviz.views
