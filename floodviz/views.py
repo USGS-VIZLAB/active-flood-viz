232d59876890344e43862f2920e18087fe414c17
skaymen <skaymen@usgs.gov>
2017-06-22 16:52:56 -0500

    many corrections

--- floodviz/views.py
+++ floodviz/views.py
@@ -11,25 +11,23 @@ from . import app
 @app.route('/home/')
 def home():
     # This program requires svgis to be installed (which in turn requires Fiona and GDAL)
-    # Takes a list of site ids in the form of "site_ids.csv" (IDs now given in config file)
-    # Takes config data in the form of "site_configs.yaml" (also now given in config file)
-    # Takes style in the form of "site_styles.css"
-    # Adds a city layer in the form of "cities.json"
-    # outputs an SVG with ids for each site
+    # Takes a list of site ids and bounding info in config file
+    # Creates an svg
 
     siteList = app.config['SITE_IDS']
     bounds = app.config['BOUNDS']
-    cities = app.config['CITIES']
+    # cities = app.config['CITIES']
 
-    # generate the string of site ids for the url
 
     # TODO: move function to utils
+    # function to generate a dictionary after querying NWIS
     def siteDict(siteList):
+
         # generate the string of site ids for the url
         id_input_string = ",".join(siteList)
 
+        # create the url
         url = app.config['NWIS_SITE_SERVICE_ENDPOINT'] + "/?format=rdb&sites=" + id_input_string + "&siteStatus=all"
-        print(url)
 
         # get data from url
         data = ""
@@ -38,9 +36,10 @@ def home():
             if not line.startswith("#"):
                 data += line + '\n'
 
+        # create array by splitting lines
         data = data.split('\n')
 
-        # make a nice dictionary from data
+        # make a list of dicts  from data
         fields = data[0].split('\t')
         dnice = []
         for line in data[2:]:
@@ -48,44 +47,43 @@ def home():
             line_dict = dict(zip(fields, line))
             dnice.append(line_dict)
 
+        # remove bad element from end of list
         dnice = dnice[:-1]
         return dnice
 
+    # list of dicts
     data_nice = siteDict(siteList)
-    # for datum in data_nice:
-    #     print(datum)
 
-    # with open("floodviz/static/data/dump.json", "w") as f:
-    #     json.dump(data_nice, f)
-    # keep only station name, id, huc, lat and lon
-    # arr = []
+    # function to write site data to a geojson file
+    # TODO: move function to utils
+    def writeGeoJson(filename, data):
+        with open(filename, "w") as f:
+            f.write("{ \"type\": \"FeatureCollection\", \"features\": [ \n")
 
-    # store this data in arr
-    # for datum in data_nice:
-    #     if 'station_nm' in datum.keys():
-    #         temp = [datum['station_nm'], datum['site_no'], datum['huc_cd'], datum['dec_lat_va'], datum['dec_long_va']]
-    #         arr.append(temp)
+            count = -1
+            for datum in data:
+                    count += 1
+                    f.write("{ \"type\": \"Feature\",\n \"geometry\": {\n \"type\": \"Point\",\n \"coordinates\" : "
+                            "[" + datum.get('dec_long_va') + ", " + datum.get('dec_lat_va') + "]\n },\n")
+                    f.write(" \"properties\": {\n \"name\": \"" + datum.get('station_nm') + "\",\n \"id\": \""
+                            + datum.get('site_no') + "\",\n \"huc\": \"" +
+                            datum['huc_cd'] + "\" \n } \n }")
+                    # add a comma unless at end of list
+                    if data[count] != data[len(data) - 1]:
+                        f.write(",")
+                    f.write("\n")
+            f.write(" ] }")
 
-    # write data to sites.json in geojson format
-    with open("floodviz/static/data/gages.json", "w") as f:
-        f.write("{ \"type\": \"FeatureCollection\", \"features\": [ \n")
+    # write data to gages.json
+    writeGeoJson("floodviz/static/data/gages.json", data_nice)
 
-        count = -1
-        for datum in data_nice:
-            # if datum.get('agency_cd') == 'USGS':
-                count += 1
-                f.write("{ \"type\": \"Feature\",\n \"geometry\": {\n \"type\": \"Point\",\n \"coordinates\" : [" + datum.get('dec_long_va') + ", " + datum.get('dec_lat_va') + "]\n },\n")
-                f.write(" \"properties\": {\n \"name\": \"" + datum.get('station_nm') + "\",\n \"id\": \"" + datum.get('site_no') + "\",\n \"huc\": \"" +
-                        datum['huc_cd'] + "\" \n } \n }")
-                if data_nice[count] != data_nice[len(data_nice) - 1]:
-                    f.write(",")
-                f.write("\n")
-        f.write(" ] }")
-
+    # write map and store in x
     x = svgis.map("floodviz/static/data/gages.json", scale=300, crs="epsg:2794", bounds=bounds)
 
+    # write map to mapout.svg
     with open("floodviz/static/data/mapout.svg", "w") as f:
         f.write(x)
+
     return render_template('index.html')
 
 
