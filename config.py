5187c6ae7bc6bb48e15a1b25960bb1ca9e5d3025
James McFeeters <jmcfeeters@usgs.gov>
2017-06-26 17:09:59 -0500

    Add basic sites map on url `/map/`
    Add template `sitemap,html`, which uses d3.js and proj4js to render
    dots representing the specified sites.
    
    In `views.py` add `sitemap`, which calls to `maputils` and renders the
    sitemap template.
    
    In `config.py` add entries for the spatialreference.org template url and
    the epsg code of the map projection to be used. Both entries are used in
    calls to `maputils`.

--- config.py
+++ config.py
@@ -2,4 +2,6 @@
 DEBUG = False
 
 NWIS_SITE_SERVICE_ENDPOINT = 'https://waterservices.usgs.gov/nwis/site'
+
 SPATIAL_REFERENCE_ENDPOINT = 'http://spatialreference.org/ref/epsg/${epsg_code}/proj4/'
+PROJECTION_EPSG_CODE = '2794'
--- floodviz/views.py
+++ floodviz/views.py
@@ -1,8 +1,17 @@
-from flask import request, render_template, abort
-
+from flask import render_template
+import json
 from . import app
 from . import maputils
+
 
 @app.route('/home/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/map/')
+def sitemap():
+    site_data = maputils.site_dict(app.config['SITE_IDS'], app.config['NWIS_SITE_SERVICE_ENDPOINT'])
+    site_data = maputils.create_geojson(site_data)
+    projection = maputils.projection_info(app.config['PROJECTION_EPSG_CODE'], app.config['SPATIAL_REFERENCE_ENDPOINT'])
+    return render_template('sitemap.html', site_data=site_data, proj4string=projection)
