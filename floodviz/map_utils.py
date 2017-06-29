69d50e20cb598e3eb4f45983b29e7557a216c16d
James McFeeters <jmcfeeters@usgs.gov>
2017-06-29 16:02:39 -0500

    Delete original `site_dict`

--- floodviz/map_utils.py
+++ floodviz/map_utils.py
@@ -5,57 +5,6 @@ from string import Template
 import requests
 
 
-def site_dict(site_list, url_prefix):
-    """
-    Puts site data into a dictionary
-
-    :param site_list: A list of site ids to be queried on NWIS
-    :param url_prefix: A string containing the beginning of the NWIS site url
-
-    :returns An array of dicts containing various site information in a usable format.
-        If service call fails, function will return None
-    """
-
-    if not site_list:
-        print("Site list empty, returning empty list")
-        return []
-
-    # generate the string of site ids for the url
-    id_input_string = ",".join(site_list)
-
-    # create the url
-    url = url_prefix + "site/?format=rdb&sites=" + id_input_string + "&siteStatus=all"
-
-    # get data from url
-    req = requests.get(url)
-
-    if req.status_code != 200:
-        print("Error: service call failed")
-        return
-
-    if req.text == "":
-        print("Service call returned no data")
-        return []
-
-    # data begins on first line that doesn't start with '#'
-    data = req.text.splitlines()
-    for line in req.text.splitlines():
-        if line.startswith('#'):
-            data.remove(line)
-        else:
-            break
-
-    # make a list of dicts from data
-    fields = data[0].split('\t')
-    dnice = []
-    for line in data[2:]:
-        line = line.split('\t')
-        line_dict = dict(zip(fields, line))
-        dnice.append(line_dict)
-
-    return dnice
-
-
 def write_geojson(filename, data):
     """
     Writes site data to a .json file so it can be mapped
