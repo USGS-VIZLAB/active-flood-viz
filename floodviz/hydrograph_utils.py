31b2086464703b84f092138e0c1b83e2de870206
skaymen <skaymen@usgs.gov>
2017-06-30 09:04:46 -0500

    changed site name to key in data

--- floodviz/hydrograph_utils.py
+++ floodviz/hydrograph_utils.py
@@ -25,7 +25,7 @@ def parse_hydrodata(jdata):
     for idx, site in enumerate(jdata):
         site_name = site['sourceInfo']['siteName']
         site_id = site['sourceInfo']['siteCode'][0]['value']
-        all_series_data.append({'key': site_id , 'values': []})
+        all_series_data.append({'key': site_id, 'name': site_name, 'values': []})
 
         # Fill new data for this series
         for idx2, obj in enumerate(site['values'][0]['value']):
@@ -39,10 +39,10 @@ def parse_hydrodata(jdata):
             dt_ms = time.mktime(dt.timetuple()) * 1000
             # (for below if statment) create dummy value for nvd3 issue at https://github.com/novus/nvd3/issues/695 #
             if idx2 is 0: # First datapoint of this site
-                all_series_data[idx]['values'].append({'site_name': site_name, 'date': date, "time": 0,
+                all_series_data[idx]['values'].append({'date': date, "time": 0,
                                                        "time_mili": dt_ms, 'value': 0})
 
-            all_series_data[idx]['values'].append({'site_name': site_name, 'date': date, "time": t,
+            all_series_data[idx]['values'].append({'date': date, "time": t,
                                                    "time_mili": dt_ms, 'value': value})
 
     return all_series_data
