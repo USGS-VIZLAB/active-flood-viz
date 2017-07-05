5a4592215463517671562202c0c544e394dc3c92
ede0m <gmoede@wisc.edu>
2017-07-05 12:46:50 -0500

    peak_value stuff

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
+
 DEBUG = False
 
-NWIS_SITE_SERVICE_ENDPOINT = 'https://waterservices.usgs.gov/nwis/'
-NWIS_WATERPEAK_SITE_SERVICE_ENDPOINT = 'https://nwis.waterdata.usgs.gov/nwis/peak'
 
-### Hydrograph Config Vars ###
+NWIS_SITE_SERVICE_ENDPOINT = 'https://waterservices.usgs.gov/nwis/'
+NWIS_PEAK_STREAMFLOW_SERVICE_ENDPOINT = 'https://nwis.waterdata.usgs.gov/nwis/peak'
+
+
+### Hydrograph Event Config Vars ###
 
 # Site IDs available for charting. Expects a list of valid site IDs.
 SITE_IDS = []
 
-# Start and end of time series data for event hydrograph
+# Start and end of time series data #
 EVENT_START_DT = ''
 EVENT_END_DT = ''
 
-
 # This dict holds aspect ratio data for hydrograph rendered in static/js/hydrograph.js. Keys are 'height' and 'width'
 HYDRO_META = {}
-
-
 
 
 
