d8bad7849cce940cded2a50bed0d668a07da75d6
ede0m <gmoede@wisc.edu>
2017-07-07 15:46:36 -0500

    units

--- floodviz/static/js/peakflow.js
+++ floodviz/static/js/peakflow.js
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				tooltip.style("display", "inline-block")
 				.style("left", (d3.event.pageX) + 10 + "px")
 				.style("top", (d3.event.pageY - 70) + "px")
-				.html((d.label) + "<br>" + (d.value) + " cfps");
+				.html((d.label) + "<br>" + (d.value) + " cfs");
 			})
 			.on("mouseout", function(d){ tooltip.style("display", "none");});
 		
@@ -108,7 +108,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				tooltip.style("display", "inline-block")
 				.style("left", (d3.event.pageX) + 10 + "px")		
 				.style("top", (d3.event.pageY) - 70 + "px")
-				.html((lolli_data.label) + "<br>" + (lolli_data.value) + " cfps");
+				.html((lolli_data.label) + "<br>" + (lolli_data.value) + " cfs");
 			})
 			.on("mouseout", function(d){ tooltip.style("display", "none");});
 		var group = d3.select('#peakflow_bar svg .group');
