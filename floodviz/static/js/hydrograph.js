80876a61f2a007ab559c3be1ec7c4e5656aed78b
skaymen <skaymen@usgs.gov>
2017-07-10 13:55:31 -0500

    wrapped in event listener

--- floodviz/static/js/hydrograph.js
+++ floodviz/static/js/hydrograph.js
@@ -1,110 +1,121 @@
-var data_path = FV.hydrograph_data_path;
+document.addEventListener("DOMContentLoaded", function (event) {
+    "use strict";
 
-// Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 30, left: 50};
-var width = FV.chart_dimensions.width - margin.left - margin.right;
-var height = FV.chart_dimensions.height - margin.top - margin.bottom;
+    var data_path = FV.hydrograph_data_path;
 
-// Set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLog().range([height, 0]);
+    // Set the dimensions of the canvas / graph
+    var margin = {top: 30, right: 20, bottom: 30, left: 50};
+    var width = FV.chart_dimensions.width - margin.left - margin.right;
+    var height = FV.chart_dimensions.height - margin.top - margin.bottom;
 
-// Define the voronoi
-var voronoi = d3.voronoi()
-	.x(function(d) { return x(d.time_mili); })
-	.y(function(d) { return y(d.value); })
-	.extent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]])
+    // Set the ranges
+    var x = d3.scaleTime().range([0, width]);
+    var y = d3.scaleLog().range([height, 0]);
 
-// Define the line
-var line = d3.line()
-	.x(function(d) { return x(d.time_mili); })
-	.y(function(d) { return y(d.value); });
+    // Define the voronoi
+    var voronoi = d3.voronoi()
+        .x(function (d) {
+            return x(d.time_mili);
+        })
+        .y(function (d) {
+            return y(d.value);
+        })
+        .extent([[-margin.left, -margin.top], [width + margin.right, height + margin.bottom]])
 
-// Adds the svg canvas
-var svg = d3.select("#hydrograph")
-	.append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform",
-		"translate(" + margin.left + "," + margin.top + ")");
+    // Define the line
+    var line = d3.line()
+        .x(function (d) {
+            return x(d.time_mili);
+        })
+        .y(function (d) {
+            return y(d.value);
+        });
 
-// Get the data
-d3.json(data_path, function(error, data) {
-	data.forEach(function (d) {
-		d.value = Number(d.value);
-	});
+    // Adds the svg canvas
+    var svg = d3.select("#hydrograph")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
-	// Scale the range of the data
-	x.domain(d3.extent(data, function (d) {
-		return d.time_mili;
-	}));
-	y.domain([20, d3.max(data, function (d) {
-		return d.value;
-	})]);
+    // Get the data
+    d3.json(data_path, function (error, data) {
+        data.forEach(function (d) {
+            d.value = Number(d.value);
+        });
 
-	// Nest the entries by site number
-	var dataNest = d3.nest()
-		.key(function (d) {
-			return d.key;
-		})
-		.entries(data);
+        // Scale the range of the data
+        x.domain(d3.extent(data, function (d) {
+            return d.time_mili;
+        }));
+        y.domain([20, d3.max(data, function (d) {
+            return d.value;
+        })]);
 
-	// Loop through each symbol / key
-	dataNest.forEach(function (d) {
+        // Nest the entries by site number
+        var dataNest = d3.nest()
+            .key(function (d) {
+                return d.key;
+            })
+            .entries(data);
 
-		svg.append("g")
-			.attr('class', 'gages')
-			.append("path")
-			.attr("id", "h" + d.key)
-			.attr("d", line(d.values))
-	});
+        // Loop through each symbol / key
+        dataNest.forEach(function (d) {
 
-	// Add the X Axis
-	svg.append("g")
-		.attr("class", "axis")
-		.attr("transform", "translate(0," + height + ")")
-		.call(d3.axisBottom(x));
+            svg.append("g")
+                .attr('class', 'gages')
+                .append("path")
+                .attr("id", "h" + d.key)
+                .attr("d", line(d.values))
+        });
 
-	// Add the Y Axis
-	svg.append("g")
-		.attr("class", "axis")
-		.call(d3.axisLeft(y).ticks(10, ".0f"));
+        // Add the X Axis
+        svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
 
-	var focus = svg.append("g")
-		.attr("transform", "translate(-100,-100)")
-		.attr("class", "focus");
-	focus.append("circle")
-		.attr("r", 3.5);
+        // Add the Y Axis
+        svg.append("g")
+            .attr("class", "axis")
+            .call(d3.axisLeft(y).ticks(10, ".0f"));
 
-	focus.append("text")
-		.attr("y", -10);
+        var focus = svg.append("g")
+            .attr("transform", "translate(-100,-100)")
+            .attr("class", "focus");
+        focus.append("circle")
+            .attr("r", 3.5);
 
-	focus.append("text")
+        focus.append("text")
+            .attr("y", -10);
 
-	var voronoiGroup = svg.append("g")
-		.attr("class", "voronoi");
+        focus.append("text")
 
-	voronoiGroup.selectAll("path")
-		.data(voronoi.polygons(d3.merge(dataNest.map(function (d) {
-			return d.values
-		}))))
-		.enter().append("path")
-		.attr("d", function (d) {
-			return d ? "M" + d.join("L") + "Z" : null;
-		})
-		.on("mouseover", mouseover)
-		.on("mouseout", mouseout);
+        var voronoiGroup = svg.append("g")
+            .attr("class", "voronoi");
 
-	function mouseover(d) {
-		d3.select(d.data.name).classed("gage--hover", true);
-		focus.attr("transform", "translate(" + x(d.data.time_mili) + "," + y(d.data.value) + ")");
-		focus.select("text").html(d.data.key + ": " + d.data.value + " cfs " + " " + d.data.time + " " + d.data.timezone);
-	}
+        voronoiGroup.selectAll("path")
+            .data(voronoi.polygons(d3.merge(dataNest.map(function (d) {
+                return d.values
+            }))))
+            .enter().append("path")
+            .attr("d", function (d) {
+                return d ? "M" + d.join("L") + "Z" : null;
+            })
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout);
 
-	function mouseout(d) {
-		d3.select(d.data.name).classed("gage--hover", false);
-		focus.attr("transform", "translate(-100,-100)");
-	}
+        function mouseover(d) {
+            d3.select(d.data.name).classed("gage--hover", true);
+            focus.attr("transform", "translate(" + x(d.data.time_mili) + "," + y(d.data.value) + ")");
+            focus.select("text").html(d.data.key + ": " + d.data.value + " cfs " + " " + d.data.time + " " + d.data.timezone);
+        }
 
+        function mouseout(d) {
+            d3.select(d.data.name).classed("gage--hover", false);
+            focus.attr("transform", "translate(-100,-100)");
+        }
+    });
 });
