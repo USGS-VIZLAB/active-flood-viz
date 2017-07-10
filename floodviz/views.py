af231a16e8d48fdcb626f1a5e75d502841ea6263
ede0m <gmoede@wisc.edu>
2017-07-10 09:27:37 -0500

    new view for data return

--- floodviz/views.py
+++ floodviz/views.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import render_template
+from flask import render_template, Response, url_for
 
 from . import app
 from . import hydrograph_utils
@@ -12,23 +12,25 @@ url_nwis_prefix = app.config['NWIS_SITE_SERVICE_ENDPOINT']
 
 @app.route('/')
 def root():
-    _hydrograph_helper()
-    _peakflow_helper()
+    peakinfo = _peakflow_helper()
     mapinfo = _map_helper()
-    return render_template('index.html', mapinfo=mapinfo)
-
+    timeseries_data_path = url_for()            # TODO: MAKE URL HERE WITH timeseries_data. Then save data on next line 
+    return render_template('index.html', mapinfo=mapinfo, peakinfo=peakinfo)
 
 @app.route('/hydrograph/')
-def home():
+def hydrograph():
     _hydrograph_helper()
     return render_template('hydrograph.html')
-
 
 @app.route('/map/')
 def sitemap():
     mapinfo = _map_helper()
     return render_template('sitemap.html', mapinfo=mapinfo)
 
+@app.route('/timeseries/')
+def timeseries_data():
+    timeseries_data = _hydrograph_helper()
+    return Response(timeseries_data, content_type='application/json')
 
 def _hydrograph_helper():
     # Hydrograph config vars #
@@ -40,9 +42,9 @@ def _hydrograph_helper():
     # Hydrodata data clean and write
     j = hydrograph_utils.req_hydrodata(sites, hydro_start_date, hydro_end_date, url_nwis_prefix)
     all_series_data = hydrograph_utils.parse_hydrodata(j)
-    with open('floodviz/static/data/hydrograph_data.json', 'w') as fout: 
-        json.dump(all_series_data, fout, indent=1)
-
+    return all_series_data    
+    # with open('floodviz/static/data/hydrograph_data.json', 'w') as fout: 
+    #     json.dump(all_series_data, fout, indent=1)
 
 
 def _peakflow_helper():
@@ -57,8 +59,7 @@ def _peakflow_helper():
     content = peak_flow_utils.req_peak_data(peak_site, peak_start_date, peak_end_date, url_peak_prefix)
     daily_value_data = peak_flow_utils.req_peak_dv_data(peak_site, peak_dv_date, url_nwis_prefix)
     peak_data = peak_flow_utils.parse_peak_data(content, daily_value_data)
-    with open('floodviz/static/data/peak_flow_data.json', 'w') as fout: 
-        json.dump(peak_data, fout, indent=1)
+    return peak_data
 
 
 
