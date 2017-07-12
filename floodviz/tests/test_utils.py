2e27f5a7eab12ce47479324d954dd441ea581c5a
ede0m <gmoede@wisc.edu>
2017-07-12 10:46:33 -0500

    valid url tests for url_construct

--- floodviz/tests/test_utils.py
+++ floodviz/tests/test_utils.py
@@ -1,40 +1,107 @@
 import unittest
 import requests_mock
-from floodviz.utils import parse_rdb
+from floodviz.utils import parse_rdb, url_construct
 
-class TestPeakUtilsRDB(unittest.TestCase):
+class TestPeakUtilsRDBparse(unittest.TestCase):
     
     def setUp(self):
-        self.test_dict = {
-                 'sites': [peak_site],
-                 'site_query': '&site_no=',
-                 'start_date': peak_start_date,
-                 'start_date_query': '&start_date=',
-                 'end_date': peak_end_date,
-                 'peak_dv_date': peak_dv_date,
-                 'end_date_query': '&end_date=',
-                 'format': '?format=rdb',
-                 'agency': '&agency_cd=USGS'
-                }
         self.peak_site = '05481950'
         self.peak_dv_date = '2008-07-05'
         self.peak_start_date = '2008-05-20'
         self.peak_end_date = '2008-07-05'
         self.endpoint = 'https://nwis.waterdata.usgs.gov/nwis/peak'
-
-class TestMapUtilsRDB(unittest.TestCase):
-    
-    def setUp():
         self.test_dict = {
-                 'sites': [peak_site],
+                 'sites': [self.peak_site],
                  'site_query': '&site_no=',
-                 'start_date': peak_start_date,
+                 'start_date': self.peak_start_date,
                  'start_date_query': '&start_date=',
-                 'end_date': peak_end_date,
-                 'peak_dv_date': peak_dv_date,
+                 'end_date': self.peak_end_date,
+                 'peak_dv_date': self.peak_dv_date,
                  'end_date_query': '&end_date=',
                  'format': '?format=rdb',
                  'agency': '&agency_cd=USGS'
                 }
+    def test_bad_request(self):
+        pass
+        # with requests_mock.Mocker() as m:
+        #     m.get(self.mock_url, status_code=404)
+        #     self.assertEqual(req_peak_data(self.site, self.P_START_DT, self.P_END_DT, self.prefix), None)
+
+class TestPeakUtilsURLconstruct(unittest.TestCase):
+    
+    def setUp(self):
+        self.peak_site = '05481950'
+        self.peak_dv_date = '2008-07-05'
+        self.peak_start_date = '2008-05-20'
+        self.peak_end_date = '2008-07-05'
+        self.endpoint_peak = 'https://nwis.waterdata.usgs.gov/nwis/peak'
+        self.endpoint_dv = 'https://waterservices.usgs.gov/nwis/dv'
+        self.url_valid_peak = self.endpoint_peak + '?format=rdb' + '&site_no=' + self.peak_site + \
+        '&start_date=' + self.peak_start_date + '&end_date=' + self.peak_end_date +'&agency_cd=USGS'
+        self.url_valid_dv = self.endpoint_dv + '?format=rdb' + '&sites=' + self.peak_site + \
+        '&startDT=' + self.peak_dv_date + '&endDT=' + self.peak_dv_date + '&siteStatus=all'
+        self.test_dict = {
+                 'sites': [self.peak_site],
+                 'site_query': '&site_no=',
+                 'start_date': self.peak_start_date,
+                 'start_date_query': '&start_date=',
+                 'end_date': self.peak_end_date,
+                 'end_date_query': '&end_date=',
+                 'format': '?format=rdb',
+                 'agency': '&agency_cd=USGS'
+                }
+        self.test_dict_dv = {
+                 'sites': [self.peak_site],
+                 'site_query': '&sites=',
+                 'start_date': self.peak_dv_date,
+                 'start_date_query': '&startDT=',
+                 'end_date': self.peak_dv_date,
+                 'end_date_query': '&endDT=',
+                 'format': '?format=rdb',
+                 'site_status': '&siteStatus=all',
+                }
+    
+    def test_valid_url_peak(self):
+        url = url_construct(self.endpoint_peak, self.test_dict)
+        self.assertEqual(url, self.url_valid_peak)
+
+    def test_valid_url_dv(self):
+        url = url_construct(self.endpoint_dv, self.test_dict_dv)
+        print(url)
+        self.assertEqual(url, self.url_valid_dv)
+
+
+
+    
+
+class TestMapUtilsRDBparse(unittest.TestCase):
+    
+    def setUp(self):
         self.sites = ['05420680', '05463500']
         self.endpoint = 'https://waterservices.usgs.gov/nwis/'
+        self.test_dict = {
+                 'sites': self.sites,
+                 'site_query': '&sites=',
+                 'site_status': '&siteStatus=all',
+                 'format': 'site/?format=rdb'
+                }
+
+
+
+class TestMapUtilsURLconstruct(unittest.TestCase):
+    
+    def setUp(self):
+        self.sites = ['05420680', '05463500']
+        self.endpoint = 'https://waterservices.usgs.gov/nwis/'
+        self.url_valid = 'https://waterservices.usgs.gov/nwis/site/?format=rdb&sites=05420680,05463500&siteStatus=all'
+        self.test_dict = {
+                 'sites': self.sites,
+                 'site_query': '&sites=',
+                 'site_status': '&siteStatus=all',
+                 'format': 'site/?format=rdb'
+                }
+    
+    def test_valid_url(self):
+        url = url_construct(self.endpoint, self.test_dict)
+        self.assertEqual(url, self.url_valid)
+        
--- floodviz/utils.py
+++ floodviz/utils.py
@@ -7,12 +7,26 @@ and returns an array of dictionaries representing the data returned in the rdb f
 """
 
 def parse_rdb(endpoint, params):
+    pass
 
+
+
+
+
+def url_construct(endpoint, params):
     ret = None
     # Construct request URL
     sites = ','.join(params['sites'])
-    url = endpoint + params['format'] + params['site_query'] + sites + params['end_date_query'] + params['end_date'] + params['agency']
-    print(url)
+    url = endpoint + params['format']
+    if params.get('site_query'):
+        url += params['site_query'] + sites
+    if params.get('start_date_query'):
+        url += params['start_date_query'] + params['start_date']
+    if params.get('end_date_query'):
+        url += params['end_date_query'] + params['end_date']
+    if params.get('site_status'):
+        url += params['site_status']
+    if params.get('agency'):
+        url += params['agency']
 
-
-    
+    return url
