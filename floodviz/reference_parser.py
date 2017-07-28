f4acb396c36364acc79645dcd4d03bf7a75aad56
skaymen <skaymen@usgs.gov>
2017-07-28 10:23:53 -0500

    Begin writing ref_parser unit tests

--- floodviz/reference_parser.py
+++ floodviz/reference_parser.py
@@ -2,35 +2,52 @@ import json
 
 # open up and reference file and store the relevant data in a dictionary
 def parse_reference_data(path):
-    with open(path, 'r') as f:
-        data = json.load(f)
+    try:
+        with open(path, 'r') as f:
+            try:
+                data = json.load(f)
 
-        parsed_data = {}
-        parsed_data['epsg'] = data['target_epsg'][5:]
-        parsed_data['site_ids'] = data['site_ids']
-        parsed_data['display_sites'] = data['display_sites']
-        parsed_data['bbox'] = data['bbox']
-        parsed_data['start_date'] = data['startDate']
-        parsed_data['end_date'] = data['endDate']
-        parsed_data['peak_dv_date'] = data['endDate']
-        parsed_data['peak_site'] = data['peak']['site']
+            except ValueError:
+                print('\n - File not valid json - \n')
+                return None
 
-        # grab the cities to be placed on the map
-        city_geojson_data = [data['reference']['features'][i] for i in range(0, len(data['reference']['features']))
-                             if data['reference']['features'][i]['properties']['reftype'] == 'city']
-        parsed_data['city_geojson_data'] = {"type": "FeatureCollection", "features": city_geojson_data}
+            try:
+                parsed_data = {}
+                parsed_data['epsg'] = data['target_epsg'][5:]
+                parsed_data['site_ids'] = data['site_ids']
+                parsed_data['display_sites'] = data['display_sites']
+                parsed_data['bbox'] = data['bbox']
+                parsed_data['start_date'] = data['startDate']
+                parsed_data['end_date'] = data['endDate']
+                parsed_data['peak_dv_date'] = data['endDate']
+                parsed_data['peak_site'] = data['peak']['site']
 
-        # grab the rivers to be placed on the map
-        river_geojson_data = [data['reference']['features'][i] for i in range(0, len(data['reference']['features']))
-                             if data['reference']['features'][i]['properties']['reftype'] == 'rivers']
-        parsed_data['river_geojson_data'] = json.dumps({"type": "FeatureCollection", "features": river_geojson_data})
+                # grab the cities to be placed on the map
+                city_geojson_data = [data['reference']['features'][i] for i in range(0, len(data['reference']['features']))
+                                     if data['reference']['features'][i]['properties']['reftype'] == 'city']
+                parsed_data['city_geojson_data'] = {"type": "FeatureCollection", "features": city_geojson_data}
 
-        # grab the political/state borders to be placed on the map
-        background_geojson_data = [data['reference']['features'][i] for i in range(0, len(data['reference']['features']))
-                             if data['reference']['features'][i]['properties']['reftype'] == 'politicalBoundaries']
-        parsed_data['background_geojson_data'] = json.dumps({"type": "FeatureCollection", "features": background_geojson_data})
+                # grab the rivers to be placed on the map
+                river_geojson_data = [data['reference']['features'][i] for i in range(0, len(data['reference']['features']))
+                                     if data['reference']['features'][i]['properties']['reftype'] == 'rivers']
+                parsed_data['river_geojson_data'] = json.dumps({"type": "FeatureCollection", "features": river_geojson_data})
 
-        return parsed_data
+                # grab the political/state borders to be placed on the map
+                background_geojson_data = [data['reference']['features'][i] for i in range(0, len(data['reference']['features']))
+                                     if data['reference']['features'][i]['properties']['reftype'] == 'politicalBoundaries']
+                parsed_data['background_geojson_data'] = json.dumps({"type": "FeatureCollection", "features": background_geojson_data})
+
+            except KeyError:
+                print('\n - Missing data - \n')
+                return None
+
+
+            return parsed_data
+
+    except FileNotFoundError:
+        print('\n - File not found - \n')
+        return None
+
 
 
 
