d7456247fe499aa0b52d8965c03d5a7767f4dd2d
skaymen <skaymen@usgs.gov>
2017-08-02 13:05:53 -0500

    Fix typo

--- floodviz/views.py
+++ floodviz/views.py
@@ -40,7 +40,7 @@ def timeseries_data():
     if thumbnail:
         with open('floodviz/thumbnail/hydrograph_data.json', 'w') as f:
             k = hydrograph_utils.req_hydrodata(ref['display_sites'], hydro_start_date, hydro_end_date, url_nwis_prefix)
-            thumbnail_data = hydrograph_utils.parse_hydrodata(j)
+            thumbnail_data = hydrograph_utils.parse_hydrodata(k)
             json.dump(thumbnail_data, f)
 
     return jsonify(timeseries_data)
