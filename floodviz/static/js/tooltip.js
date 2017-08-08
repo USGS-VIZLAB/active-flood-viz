9d95821eabc7dfca7d0e760f57491c27af18c624
James McFeeters <jameeters@gmail.com>
2017-08-08 11:17:20 -0500

    Clean up tooltip.js
    Factor out some of the logic from `show_tooltip` into new functions.

--- floodviz/static/js/tooltip.js
+++ floodviz/static/js/tooltip.js
@@ -1,37 +1,42 @@
 'use strict';
 const tooltip_module = function (options) {
 	var self = {};
+
 	/**
-	 *
-	 * @param elements {Object} containing pointers to the SVG elements used in the tooltip:
-	 *    group: the group containing the other tooltip elements
-	 *    text: the text to be shown
-	 *    backdrop: the rectangle used as a background for the text
-	 *    arrow: the triangle thing
-	 *
-	 * @param textstring {string} the text to be used in the tooltip
-	 * @param edges {object} of the form {r: , l: , t: } giving the  right, left, and top edges of the figure
-	 * @param center {object} of the form {x: , y: } giving the SVG coordinates to be used as the center of the tooltip.
-	 * @param visible_class {string} the name of the class to which the tooltip is set to make it visible
+	 * Takes arrow height and the calculated adjustment (to tell whether or not to invert the arrow)
+	 * and returns the points-string for the arrow.
+	 * @param height
+	 * @param adjust
+	 * @returns a string of points to make up the arrow
 	 */
-	self.show_tooltip = function (elements, textstring, edges, center, visible_class) {
-		const padding = 4;
-		const arrowheight = 17;
+	const make_arrow = function(height, adjust){
 		// sin 60 = sqrt(3)/2 =~ 0.866. Ie, an equilateral triangle of height sqrt(3) will have sides of length 2.
 		// So an equilateral triangle with height x will have sides of length x / 0.866
-		const sidelength = arrowheight / 0.866;
+		const sidelength = height / 0.866;
 
+		const points = [[0, 0], [-(sidelength / 2), -adjust.t * height], [(sidelength / 2), -adjust.t * height], [0, 0]];
 
-		elements.group.attr('transform', 'translate(' + center.x + ', ' + center.y + ')')
-			.attr('class', visible_class);
+		// turn points array into string
+		var arrowpoints = '';
+		points.forEach(function (p) {
+			arrowpoints += p[0] + ' ' + p[1] + ',';
+		});
+		arrowpoints = arrowpoints.substring(0, arrowpoints.length - 1);
+		return arrowpoints;
+	};
 
-
-		// I have to set the text before I can check if it collides with the edges,
-		// but I can check if it collides with the top without bumping it up; I only use its height.
-		elements.text.html(textstring);
-
-		const textbound = elements.text._groups[0][0].getBBox();
-
+	/**
+	 * Calculates what (if any) adjustments are needed to keep the tooltip from running off the sides or top of the svg.
+	 *
+	 * @param textbound {object} The bounding box around the text
+	 * @param center {object} The location of the thing on which the tooltip is centered
+	 * @param edges {object} The locations of the edges of the figure, with which the tooltip should not collide
+	 * @param arrowheight {int} The height of the arrow under the tooltip
+	 *
+	 * @returns {{l: number, r: number, t: number}} The left and right adjustment required, and whether or not to draw
+	 * the tooltip inverted. See 'EXPLANATION OF `t`' below for more.
+	 */
+	const calculate_adjust = function(textbound, center, edges, arrowheight){
 		// remember that (0, 0) is in the upper left corner
 		const tipedges = {
 			l: center.x - textbound.width / 2,
@@ -64,16 +69,40 @@ const tooltip_module = function (options) {
 			// set t to -1 so that the tooltip will bw drawn under the gage.
 			adjust.t = -1
 		}
+		return adjust;
+	};
 
-		const points = [[0, 0], [-(sidelength / 2), -adjust.t * arrowheight], [(sidelength / 2), -adjust.t * arrowheight], [0, 0]];
+	/**
+	 * Displays the tooltip on a figure.
+	 *
+	 * @param elements {Object} containing pointers to the SVG elements used in the tooltip:
+	 *    group: the group containing the other tooltip elements
+	 *    text: the text to be shown
+	 *    backdrop: the rectangle used as a background for the text
+	 *    arrow: the triangle thing
+	 *
+	 * @param textstring {string} the text to be used in the tooltip
+	 * @param edges {object} of the form {r: , l: , t: } giving the  right, left, and top edges of the figure
+	 * @param center {object} of the form {x: , y: } giving the SVG coordinates to be used as the center of the tooltip.
+	 * @param visible_class {string} the name of the class to which the tooltip is set to make it visible
+	 */
+	self.show_tooltip = function (elements, textstring, edges, center, visible_class) {
+		const padding = 4;
+		const arrowheight = 17;
 
-		// turn points array into string
-		var arrowpoints = '';
-		points.forEach(function (p) {
-			arrowpoints += p[0] + ' ' + p[1] + ',';
-		});
-		arrowpoints = arrowpoints.substring(0, arrowpoints.length - 1);
+		elements.group.attr('transform', 'translate(' + center.x + ', ' + center.y + ')')
+			.attr('class', visible_class);
 
+
+		// I have to set the text before I can check if it collides with the edges,
+		// but I can check if it collides with the top without bumping it up; I only use its height.
+		elements.text.html(textstring);
+
+		const textbound = elements.text._groups[0][0].getBBox();
+
+		const adjust = calculate_adjust(textbound, center, edges, arrowheight);
+
+		const arrowpoints = make_arrow(arrowheight, adjust);
 		elements.arrow.attr('points', arrowpoints);
 
 		elements.text.attr('y', (-adjust.t * (arrowheight + padding * 2)));
